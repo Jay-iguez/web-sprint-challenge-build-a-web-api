e988d091ca37b63832a513ed925636ad19878f94
Jayden Rodriguez <02rodriguez.j@gmail.com>
2023-11-14 18:10:36 -0700

    Create final get/:id/actions, post/, put/:id, and delete/:id with middleware checkBody

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,6 +1,10 @@
 // add middlewares here related to projects
 const data = require('./projects-model')
 
+const checkTypeOf = (value, type) => {
+    return typeof value === type ? true : false
+}
+
 async function checkId (req, res, next) {
     const { id } = req.params
 
@@ -15,15 +19,18 @@ async function checkId (req, res, next) {
     }
 }
 
-/**
- * function checkBody (req, res, next) {
+function checkBody (req, res, next) {
     const { name, description, completed} = req.body
 
-
+    if (checkTypeOf(name, 'string') && checkTypeOf(description, 'string') && checkTypeOf(completed, 'boolean')) {
+        next()
+    } else {
+        const error = new Error('Body of request invalid - must include name, description, and completed!')
+        error.status = 400
+        next(error)
+    }
 }
- */
-
 
 module.exports = {
-    checkId
+    checkId, checkBody
 }
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,6 +1,6 @@
 // Write your "projects" router here!
 const data = require('./projects-model')
-const { checkId } = require('./projects-middleware')
+const { checkId, checkBody } = require('./projects-middleware')
 const express = require('express')
 
 const projects = express.Router()
@@ -22,17 +22,28 @@ projects.get('/:id', checkId, async (req, res, next) => {
         const project = await data.get(id)
         res.status(200).json(project)
     } catch (err) {
-        res.locals.error = `Error getting project of id: ${id}`
+        res.locals.error = `Error getting project of id: ${req.params.id}`
         err.status = 500
         next(err)
     }
 })
 
-/**
- * 
- * projects.post('/', async (req, res, next) => {
+projects.get('/:id/actions', checkId, async (req, res, next) => {
     try {
+        const { id } = req.params
+        const actions = await data.getProjectActions(id)
+        res.status(200).json(actions)
+    } catch (err) {
+        res.locals.error = `Error in getting project actions of id: ${req.params.id}`
+    }
+})
 
+projects.post('/', checkBody, async (req, res, next) => {
+    try {
+        const { name, description, completed } = req.body
+        const body = { name: name, description: description, completed: completed }
+        const newProject = await data.insert(body)
+        res.status(201).json(newProject)
     } catch (err) {
         res.locals.error = "Error posting project"
         err.status = 500
@@ -40,7 +51,31 @@ projects.get('/:id', checkId, async (req, res, next) => {
     }
 })
 
- */
+projects.put('/:id', [checkId, checkBody], async (req, res, next) => {
+    try {
+        const { id } = req.params
+        const { name, description, completed } = req.body
+        const body = { name: name, description: description, completed: completed }
+        const updatedProject = await data.update(id, body)
+        res.status(200).json(updatedProject)
+    } catch (err) {
+        res.locals.error = `Error in updating project of id: ${req.params.id}`
+        err.status = 500
+        next(err)
+    }
+})
+
+projects.delete('/:id', checkId, async (req, res, next) => {
+    try {
+        const { id } = req.params
+        await data.remove(id)
+        res.status(200).end()
+    } catch (err) {
+        res.locals.error = `Error in deleting project of id: ${req.params.id}`
+        err.status = 500
+        next(err)
+    }
+})
 
 projects.use((err, req, res, next) => {
     res.status(err.status).json({
