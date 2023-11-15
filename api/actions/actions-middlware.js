0a05277b52691af814159da6eea03e2a42d6e588
Jayden Rodriguez <02rodriguez.j@gmail.com>
2023-11-14 19:19:12 -0700

    Create final post/, put/:id, and delete/:id in actions-router and middleware of checkBody

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -1,6 +1,10 @@
 // add middlewares here related to actions
 const data = require('./actions-model')
 
+const checkTypeOf = (value, type) => {
+    return typeof value === type ? true : false
+}
+
 async function checkId(req, res, next) {
     const {id} = req.params
 
@@ -15,6 +19,18 @@ async function checkId(req, res, next) {
     }
 }
 
+function checkBody(req, res, next) {
+    const { project_id, description, notes, completed = false } = req.body
+
+    if (checkTypeOf(project_id, 'number') && checkTypeOf(description, 'string') && checkTypeOf(notes, 'string') && checkTypeOf(completed, 'boolean')) {
+        next()
+    } else {
+        const error = new Error('Body of request invalid - must include project id, description, and notes! Completed property optional.')
+        error.status = 400
+        next(error)
+    }
+}
+
 module.exports = {
-    checkId
+    checkId, checkBody
 }
--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,6 +1,6 @@
 // Write your "actions" router here!
 const data = require('./actions-model.js')
-const { checkId } = require('./actions-middlware.js')
+const { checkId, checkBody } = require('./actions-middlware.js')
 const express = require('express')
 
 const actions = express.Router()
@@ -18,11 +18,52 @@ actions.get('/', async (req, res, next) => {
 
 actions.get('/:id', checkId, async (req, res, next) => {
     try {
-        const {id} = req.params
+        const { id } = req.params
         const action = await data.get(id)
         res.status(200).json(action)
-    } catch(err) {
+    } catch (err) {
         res.locals.error = `Error in getting action of id ${req.params.id}`
+        err.status = 500
+        next(err)
+    }
+})
+
+actions.post('/', checkBody, async (req, res, next) => {
+    try {
+        const { project_id, description, notes, completed = false } = req.body
+        const body = { project_id: project_id, description: description, notes: notes, completed: completed }
+        const newAction = await data.insert(body)
+        res.status(201).json(newAction)
+    } catch (err) {
+        res.locals.error = 'Error in creating new action'
+        err.status = 500
+        next(err)
+    }
+})
+
+actions.put('/:id', [checkId, checkBody], async (req, res, next) => {
+    try {
+        const { id } = req.params
+        const { project_id, description, notes, completed = false } = req.body
+        const body = { project_id: project_id, description: description, notes: notes, completed: completed }
+        const updatedAction = await data.update(id, body)
+        res.status(200).json(updatedAction)
+    } catch (err) {
+        res.locals.error = `Error in updating action of id ${req.params.id}`
+        err.status = 500
+        next(err)
+    }
+})
+
+actions.delete('/:id', checkId, async (req, res, next) => {
+    try {
+        const { id } = req.params
+        await data.remove(id)
+        res.status(200).end()
+    } catch (err) {
+        res.locals.error = `Error in deleting action of id ${req.params.id}`
+        err.status = 500
+        next(err)
     }
 })
 
