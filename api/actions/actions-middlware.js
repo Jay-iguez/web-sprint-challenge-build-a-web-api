d3ebc3ac3a443acc38ecb99cb3d44ff829bc0a93
Jayden Rodriguez <02rodriguez.j@gmail.com>
2023-11-14 19:00:20 -0700

    Create get/ and get/:id along with checkId middleware of actions-router

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -1,1 +1,20 @@
 // add middlewares here related to actions
+const data = require('./actions-model')
+
+async function checkId(req, res, next) {
+    const {id} = req.params
+
+    const actions = await data.get(id)
+
+    if (actions !== null) {
+        next()
+    } else {
+        const error = new Error(`Action of ${id} does not exist!`)
+        error.status = 404
+        next(error)
+    }
+}
+
+module.exports = {
+    checkId
+}
--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,22 +1,34 @@
 // Write your "actions" router here!
 const data = require('./actions-model.js')
+const { checkId } = require('./actions-middlware.js')
 const express = require('express')
 
 const actions = express.Router()
 
 actions.get('/', async (req, res, next) => {
     try {
-        res.status(200).json({
-            message: "The sand gives way to even the gentlest of steps"
-        })
+        const actions = await data.get()
+        res.status(200).json(actions)
     } catch (err) {
+        res.locals.error = 'Error in getting actions'
+        err.status = 500
+        next(err)
+    }
+})
 
+actions.get('/:id', checkId, async (req, res, next) => {
+    try {
+        const {id} = req.params
+        const action = await data.get(id)
+        res.status(200).json(action)
+    } catch(err) {
+        res.locals.error = `Error in getting action of id ${req.params.id}`
     }
 })
 
 actions.use((error, req, res, next) => {
     res.status(error.status).json({
-        message: `[${res.locals.error}]: ${error.message}`
+        message: `[${res.locals.error || 'Middleware error'}]: ${error.message}`
     })
 })
 
